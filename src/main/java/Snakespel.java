6d50fa352384dad4c18777b31665583eb53271c5
EmielBertrand <emielbertrand@gmail.com>
2022-10-18 16:15:44 +0200

    w

---
+++ src/main/java/Snakespel.java
@@ -0,0 +1,19 @@
+
+import com.github.hanyaeger.api.YaegerGame;
+
+import static javafx.application.Application.launch;
+
+public class Snakespel extends YaegerGame {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void setupScenes() {
+        
+    }
+}
